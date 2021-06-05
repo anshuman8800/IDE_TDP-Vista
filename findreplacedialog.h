a4f1621a46431e338fbf9a8d3ef386537b42ee77
Anshuman <anshuman8800@gmail.com>
2021-06-05 23:24:02 +0530

    Updated header files for texteditor

---
+++ findreplacedialog.h
@@ -0,0 +1,14 @@
+#ifndef FINDREPLACEDIALOG_H
+#define FINDREPLACEDIALOG_H
+
+
+
+class FindReplaceDialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    FindReplaceDialog();
+};
+
+#endif // FINDREPLACEDIALOG_H
